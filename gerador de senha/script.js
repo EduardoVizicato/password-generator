593b082277dbeeaebc6a3f940c8dcae05a64c1e2
Eduardo Vizicato <104400559+EduardoVizicato@users.noreply.github.com>
2023-07-04 19:14:24 -0300

    Add files via upload

---
+++ gerador de senha/script.js
@@ -0,0 +1,36 @@
+let sliderElement = document.getElementById('slider');
+let buttonElement = document.getElementById('button');
+
+let sizePassword = document.getElementById ('valor');
+let password = document.getElementById('password');
+
+let containerPassword = document.getElementById('container-password');
+
+let charset = "aeiouywbcdfghjklmnpqrstvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword (){
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+        
+    }
+
+    containerPassword.classList.remove("hide")
+
+    password.innerHTML = pass;
+
+    novaSenha = pass;
+
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(novaSenha);
+}
